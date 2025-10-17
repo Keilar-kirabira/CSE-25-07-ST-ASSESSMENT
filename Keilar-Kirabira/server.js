10ea9507dd1ab0a3d8a2d9edb3e6a385f1765ff0
keilar kirabira <kirabirakeilarlisa@gmail.com>
2025-10-17 12:09:44 -0700

    created other files and imported the dependencies

---
+++ Keilar-Kirabira/server.js
@@ -0,0 +1,95 @@
+//1.Dependencies
+const express = require("express");
+const path = require("path");
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressSession = require('express-session');
+const MongoStore = require("connect-mongo");
+
+
+
+
+require('dotenv').config();
+const UserModel = require("./models/userModel");
+//import routes
+const authRoutes  = require("./routes/authRoutes");
+//2.Instantiations
+const app = express();
+const port = 3000;
+
+//3.Configurations
+app.locals.moment = moment;
+//settingup mongodb connections
+mongoose.connect(process.env.MONGODB_URL, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+  
+  // setting view engine to pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//4.Middleware
+//method override
+app.use(methodOverride('_method'));
+// app.use(express.static('public'));   //static files
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true })); // helps to pass data from forms
+//express session configs
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({mongoUrl:process.env.MONGODB_URL}),
+  cookie: {maxAge:24*60*60*1000}    //oneday
+}))
+//passport configs
+app.use(passport.initialize());
+app.use(passport.session());
+
+//authenticate with passport local strategy
+passport.use(UserModel.createStrategy());
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser()); 
+
+
+
+
+
+
+
+//5.Routes
+app.use("/",authRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//non existent route handler
+app.use((req, res) => {
+  res.status(404).send("Oops! Route not found.");
+});
+
+//6.Bootstrapping Server
+app.listen(port, () => console.log(`listening on port ${port} `));
